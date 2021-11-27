a719a7c0691e68f0faa154ea7a6684636c3cd30d
ysong02 <71662449+ysong02@users.noreply.github.com>
2021-11-27 12:07:48 +0100

    Add files via upload

---
+++ C Language/Project_TCP_UDP/server3_00.c
@@ -0,0 +1,290 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/select.h>
+#include<sys/time.h>
+#include<unistd.h>
+#include<netinet/in.h>
+
+#define RECVSIZE 1024
+
+int main(int argc, char *argv[]){
+    //Verification of pamameters
+    //Execute in terminal ./server1-LePic NumeroPort
+    if(argc<2){
+        perror("./server1-LePic NumeroPort\n");
+        return 0;
+    }
+
+    //Initialization
+    int port = atoi(argv[1]);
+    char sendmsg[RECVSIZE];
+    char recvmsg[RECVSIZE];
+    //char transmsg[RECVSIZE];
+
+    //Creat Socket for Server's Controler
+    int socketC = socket(AF_INET,SOCK_DGRAM,0);
+    if(socket<0){
+        perror("ERROR: Cannot creat socket for Server's Controler.\n");
+        return -1;
+    }
+
+    int validC = 1;
+    setsockopt(socketC, SOL_SOCKET,SO_REUSEADDR,&validC,sizeof(int));
+
+    //Creat Server' Controler Address
+    struct sockaddr_in addressC;
+    addressC.sin_family = AF_INET;
+    addressC.sin_port = htons(port);
+    addressC.sin_addr.s_addr = htonl(INADDR_ANY);
+    socklen_t lenC = sizeof(addressC);
+
+    //Bind the Server's Controler
+    if(bind(socketC, (struct sockaddr *)&addressC, lenC)==-1){
+        perror("Bind for Server's Controler Failed.\n");
+        return -1;
+    }
+
+
+
+    //Creat Client's Address
+    struct sockaddr_in addressClient;
+    socklen_t lenClient = sizeof(addressClient);
+
+    while(1){
+        //Handshake
+        memset(recvmsg, 0, RECVSIZE);
+        recvfrom(socketC, recvmsg, RECVSIZE, 0, (struct sockaddr *)&addressClient, &lenClient);
+        if(strcmp(recvmsg, "SYN")==0){
+            printf("Server: HANDSHAKE 1 Recieved.\n");
+            memset(sendmsg, 0, RECVSIZE);
+            strcpy(sendmsg, "SYN-ACK");
+            //Calcul the new port
+            char portString[5];
+            port ++;
+            sprintf(portString, "%4d", port);
+            strcat(sendmsg,portString);
+            sendto(socketC, sendmsg, RECVSIZE, 0, (struct sockaddr *)&addressClient, lenClient);
+            printf("Server: HANDSHAKE 2 Sended.\n");
+        }else if(strcmp(recvmsg, "ACK")==0){
+            printf("Server: HANDSHAKE 3 Recieved.\n");
+
+            pid_t fpid;
+            fpid = fork();
+            if(fpid == 0){
+              close(socketC);
+              //Creat Socket for Client
+        	    int socketU = socket(AF_INET,SOCK_DGRAM,0);
+        	    if(socket<0){
+          		  perror("ERROR: Cannot creat socket for Client.\n");
+          		  return -1;
+              }
+
+        	    int validU = 1;
+        	    setsockopt(socketU, SOL_SOCKET, SO_REUSEADDR, &validU, sizeof(int));
+
+        	    //Creat Socket Client's Address
+        	    struct sockaddr_in addressU;
+        	    addressU.sin_family = AF_INET;
+        	    addressU.sin_addr.s_addr = htonl(INADDR_ANY);
+              addressU.sin_port = htons(port);
+        	    socklen_t lenU = sizeof(addressU);
+
+              if(bind(socketU, (struct sockaddr *)&addressU,lenU)==-1){
+                  perror("Bind for Client Failed.\n");
+                  close(socketU);
+                  close(socketC);
+                  return -1;
+              }
+
+              //Open the File
+              memset(recvmsg, 0, RECVSIZE);
+              recvfrom(socketU, recvmsg, RECVSIZE, 0, (struct sockaddr *)&addressClient, &lenClient);
+              FILE *fp = fopen(recvmsg, "r");
+              if(fp == NULL){
+                  memset(sendmsg, 0, RECVSIZE);
+                  strcpy(sendmsg, "Cannot Find Your File!");
+                  printf("Cannot find the file!\n");
+                  sendto(socketU, sendmsg, RECVSIZE, 0, (struct sockaddr *)&addressClient, lenClient);
+              }else{
+                  //Creat Sequence Number
+                  int seqRecv = 0;
+                  int seqSend = 1;
+                  int endFile = 0;
+
+                  //Creat Window Size
+                  int wSizeCste = 1;
+                  int wOccupiedSize = 0;
+
+                  //Creat Congestion Controle
+                  int ssthred=64;
+                  int congst = 0;
+                  float wSizeFloat = 1.0;
+
+
+                  //Tansmission of File
+                  char buffer[RECVSIZE-6];
+                  memset(buffer, 0, RECVSIZE-6);
+                  int len = 1;
+                  int lenEnd = 0;
+                  if((len = fread(buffer, sizeof(char), RECVSIZE-6, fp))!=0){
+                    lenEnd = len;
+                    memset(sendmsg, 0, RECVSIZE);
+                    sprintf(sendmsg,"%.6d",seqSend);
+                    memcpy(sendmsg+6, buffer, len);
+                    //Send to Client
+                    printf("SEND TO CLIENT: Segement %.6d, Size: %ld \n", seqSend, sizeof(sendmsg));
+                    sendto(socketU, sendmsg, len+6, 0,(struct sockaddr *)&addressClient, lenClient);
+                    seqSend=seqSend+1;
+                    wOccupiedSize = wOccupiedSize+1;
+                    memset(buffer, 0, RECVSIZE-6);
+                  }
+
+                  //Parameters of select
+                  fd_set fds;
+                  struct timeval timeout={3,0};
+
+                  //Fast Recovery
+                  int seqDup = 0;
+                  int nSeqDup = 0;
+
+                  while(endFile ==0){
+                    FD_ZERO(&fds);
+                    FD_SET(socketU, &fds);
+                    int maxfdp = socketU+1;
+                    timeout.tv_sec = 3;
+                    switch(select(maxfdp,&fds,NULL,NULL,&timeout)){
+                      case -1: exit(-1);
+                        break;
+                      case 0:
+
+                        printf("\nTIMEOUT!!! Fast Retransmit Here!\n");
+
+                        fseek(fp, -(wOccupiedSize-1)*(RECVSIZE-6)-lenEnd, SEEK_CUR);
+                        seqSend  = seqSend - wOccupiedSize;
+                        wOccupiedSize = 0;
+                        if(congst == 1){
+                          ssthred = (int)wSizeCste/2;
+                          congst = 0;
+                        }
+                        wSizeCste = 1;
+                        wSizeFloat = wSizeCste;
+                        while((wOccupiedSize<wSizeCste)&&((len = fread(buffer, sizeof(char), RECVSIZE-6, fp))!=0)){
+                          lenEnd = len;
+                          memset(sendmsg, 0, RECVSIZE);
+                          sprintf(sendmsg,"%.6d",seqSend);
+                          memcpy(sendmsg+6, buffer, len);
+                          printf("SEND TO CLIENT: Segement %.6d, Size: %d \n", seqSend, len);
+                          sendto(socketU, sendmsg, len+6, 0,(struct sockaddr *)&addressClient, lenClient);
+                          seqSend=seqSend+1;
+                          wOccupiedSize = wOccupiedSize + 1;
+                          memset(buffer, 0, RECVSIZE-6);
+                        }
+                        break;
+                      default:
+                        if(FD_ISSET(socketU,&fds)){
+                          if(wSizeCste>=ssthred){
+                            congst = 1;
+                          }
+                          if(congst == 0){
+                            wSizeCste = wSizeCste + 1;
+                            wSizeFloat = wSizeCste;
+                          }else{
+                            wSizeFloat = wSizeFloat + 1.0/wSizeCste;
+                            wSizeCste = (int)wSizeFloat;
+                          }
+                          memset(recvmsg, 0, RECVSIZE);
+                          recvfrom(socketU, recvmsg, 9, 0, (struct sockaddr *)&addressClient, &lenClient);
+                          printf("RECV ACK: %s\n", recvmsg);
+                          char seqRecvChar[7];
+                          strncpy(seqRecvChar,recvmsg+4,6);
+                          seqRecv = atoi(seqRecvChar);
+                          if(seqRecv == seqSend-wOccupiedSize){
+                            wOccupiedSize = wOccupiedSize - 1;
+                            while((wOccupiedSize<wSizeCste)&&((len = fread(buffer, sizeof(char), RECVSIZE-6, fp))!=0)){
+                              lenEnd = len;
+                              memset(sendmsg, 0, RECVSIZE);
+                              sprintf(sendmsg,"%.6d",seqSend);
+                              memcpy(sendmsg+6, buffer, len);
+                              printf("SEND TO CLIENT: Segement %.6d, Size: %d \n", seqSend, len);
+                              sendto(socketU, sendmsg, len+6, 0,(struct sockaddr *)&addressClient, lenClient);
+                              seqSend=seqSend+1;
+                              wOccupiedSize = wOccupiedSize+1;
+                              memset(buffer, 0, RECVSIZE-6);
+                            }
+                          }else if(seqRecv < seqSend-wOccupiedSize){
+                            //Loss of Packet
+                            if(seqDup == seqRecv){
+                              nSeqDup = nSeqDup + 1;
+                            }else{
+                              seqDup = seqRecv;
+                              nSeqDup = 0;
+                            }
+                            if(nSeqDup >= 3){
+                              nSeqDup = 0;
+
+                              printf("\n3 Duplicated ACK!!! Fast Recovery Here! \n");
+
+                              ssthred = (int)wSizeCste/2;
+                              congst = 1;
+                              wSizeCste = ssthred + 3;
+                              wSizeFloat = wSizeCste;
+                              fseek(fp, -(wOccupiedSize-1)*(RECVSIZE-6)-lenEnd, SEEK_CUR);
+                              seqSend = seqSend - wOccupiedSize;
+                              wOccupiedSize = 0;
+                              while((wOccupiedSize<wSizeCste)&&((len = fread(buffer, sizeof(char), RECVSIZE-6, fp))!=0)){
+                                lenEnd = len;
+                                memset(sendmsg, 0, RECVSIZE);
+                                sprintf(sendmsg,"%.6d",seqSend);
+                                memcpy(sendmsg+6, buffer, len);
+                                printf("SEND TO CLIENT: Segement %.6d, Size: %d \n", seqSend, len);
+                                sendto(socketU, sendmsg, len+6, 0,(struct sockaddr *)&addressClient, lenClient);
+                                seqSend=seqSend+1;
+                                wOccupiedSize = wOccupiedSize + 1;
+                                memset(buffer, 0, RECVSIZE-6);
+                              }
+                            }
+
+                          }else{
+                            //After refind the lost packet
+                            wOccupiedSize = seqSend - seqRecv - 1;
+                            while((wOccupiedSize<wSizeCste)&&((len = fread(buffer, sizeof(char), RECVSIZE-6, fp))!=0)){
+                              lenEnd = len;
+                              memset(sendmsg, 0, RECVSIZE);
+                              sprintf(sendmsg,"%.6d",seqSend);
+                              memcpy(sendmsg+6, buffer, len);
+                              //Send to Client
+                              printf("SEND TO CLIENT: Segement %.6d, Size: %d \n", seqSend, len);
+                              sendto(socketU, sendmsg, len+6, 0,(struct sockaddr *)&addressClient, lenClient);
+                              seqSend=seqSend+1;
+                              wOccupiedSize = wOccupiedSize+1;
+                              memset(buffer, 0, RECVSIZE-6);
+                            }
+                          }
+                          if((len==0)&&(wOccupiedSize==0)){
+                            endFile = 1;
+                          }
+                        }
+                    }
+
+                  }
+
+                }
+                memset(sendmsg, 0, RECVSIZE);
+                strcpy(sendmsg,"FIN");
+                printf("FIN\n");
+                sendto(socketU, sendmsg, RECVSIZE, 0,(struct sockaddr *)&addressClient, lenClient);
+    	          close(socketU);
+                return 0;
+            }
+
+        }else{
+            perror("ERROR: UNKNOW ERROR\n");
+            return -1;
+        }
+    }
+    close(socketC);
+    return 0;
+}
